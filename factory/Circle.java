c57e5dc23eb198868af7778b182e8b4f6c9e5941
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-25 17:48:24 +0530

    Create Circle.java

---
+++ factory/Circle.java
@@ -0,0 +1,10 @@
+package com.saftware.pattern.factory;
+
+public class Circle implements Shape {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm a circle");
+	}
+
+}
