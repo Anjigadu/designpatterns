624e4632c7c5e69dddd196f62062f270a7314ff7
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-25 18:13:38 +0530

    Create SingletonTest.java

---
+++ singleton/test/SingletonTest.java
@@ -0,0 +1,16 @@
+package com.saftware.pattern.singleton;
+
+public class SingletonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleObject singleObject = SingleObject.getSingleObject();
+		
+		
+		singleObject.sayHello();
+	}
+
+}
