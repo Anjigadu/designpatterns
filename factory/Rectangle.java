6a3e498b812fe64d7dd804d64818a41ca07a2698
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-25 17:48:13 +0530

    Create Rectangle.java

---
+++ factory/Rectangle.java
@@ -0,0 +1,10 @@
+package com.saftware.pattern.factory;
+
+public class Rectangle implements Shape {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm a rectangle");
+	}
+
+}
