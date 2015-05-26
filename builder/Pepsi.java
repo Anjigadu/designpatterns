03aa9835cea1d9be90e49e019c53295731bc6d4c
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-26 11:42:55 +0530

    Create Pepsi.java

---
+++ builder/Pepsi.java
@@ -0,0 +1,13 @@
+package com.saftware.pattern.builder;
+
+public final class Pepsi extends ColdDrink {
+
+	public String name() {
+		return "Name: Pepsi";
+	}
+
+	public float price() {
+		return 2.5f;
+	}
+
+}
