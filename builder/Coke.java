4e57cc5cdfa38a7eaadd771ae00f554c27eacc73
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-26 11:41:18 +0530

    Create Coke.java

---
+++ builder/Coke.java
@@ -0,0 +1,14 @@
+package com.saftware.pattern.builder;
+
+public final class Coke extends ColdDrink {
+
+	public String name() {
+		return "Name: Coke";
+	}
+
+	public float price() {
+
+		return 2.5f;
+	}
+
+}
