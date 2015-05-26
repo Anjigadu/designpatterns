bdd37e5be03cae026a837492302cad04d5382425
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-26 11:42:44 +0530

    Create NonVegBurger.java

---
+++ builder/NonVegBurger.java
@@ -0,0 +1,14 @@
+package com.saftware.pattern.builder;
+
+public final class NonVegBurger extends Burger {
+
+	public String name() {
+		return "Name: Non Veg Burger";
+	}
+
+	public float price() {
+
+		return 4.5f;
+	}
+
+}
