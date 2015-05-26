ce71d760ae177f69a878d1fd7a804ac46b17bcde
Sathiyaseelan <ssstarlet@gmail.com>
2015-05-26 11:43:09 +0530

    Create VegBurger.java

---
+++ builder/VegBurger.java
@@ -0,0 +1,14 @@
+package com.saftware.pattern.builder;
+
+public final class VegBurger extends Burger {
+
+	public String name() {
+		return "Name: Veg Burger";
+	}
+
+	public float price() {
+		// TODO Auto-generated method stub
+		return 2.5f;
+	}
+
+}
